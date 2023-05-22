80f4d34cf0e97e062e7f780f237da273882c3fd0
Laramaggioli2 <126704260+Laramaggioli2@users.noreply.github.com>
2023-05-22 13:31:03 -0300

    Add files via upload

---
+++ ejercicio52.py
@@ -0,0 +1,13 @@
+espacio=" "
+contador=0
+oracion = input("Ingrese una oración: ")
+if espacio in oracion:
+    contador=contador+1
+    print("Se ingresaron ",espacio1," espacios.")
+cadena1=oracion.upper();
+print(cadena1)
+cadena2=oracion.lower();
+print(cadena2)
+cadena3=oracion.capitalize();
+print(cadena3)
+
